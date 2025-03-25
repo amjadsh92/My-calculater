99c24fb9f09b102b2f5606c37b8f77446cbcc835
amjadsh92 <amjadsharafeddien@gmail.com>
2025-03-25 17:18:02 +0200

    Initialize the files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+/* eslint-disable */
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
+
+
+function App() {
+  
+
+}
