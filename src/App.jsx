a4e67e9c0e430cae2ab81e9fe4b0a167eabeeee0
amjadsh92 <amjadsharafeddien@gmail.com>
2025-03-25 21:11:13 +0200

    Design the calculater

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
 
 /* eslint-disable */
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 import { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 
 function App() {
+
+
+
+  return(
+      <div className="app">
+       <div className="calculater">
+        <div className="input">
+          <div id="seven" class="number">7</div>
+          <div id="eight"  class="number">8</div>
+          <div id="nine"  class="number">9</div>
+          <div id="minus"  class="operation">-</div>
+          <div id="seven" class="number">4</div>
+          <div id="eight"  class="number">5</div>
+          <div id="nine"  class="number">6</div>
+          <div id="minus"  class="operation">+</div>
+          <div id="one" class="number">1</div>
+          <div id="two"  class="number">2</div>
+          <div id="three"  class="number">3</div>
+          <div id="equal"  class="equal">=</div>
+          <div id="zero"  class="zero">0</div>
+          <div id="dot"  class="dot">.</div>
+
+        </div>
+         
+
+       </div>
+
+      </div>
+
+  )
   
 
 }
+
+
+export default App
