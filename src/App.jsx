a8bfb02f9251d4dec98a3222b23ab2231eab2410
amjadsh92 <amjadsharafeddien@gmail.com>
2025-03-26 10:08:19 +0200

    Design the calculater

--- src/App.jsx
+++ src/App.jsx
@@ -13,20 +13,23 @@ function App() {
       <div className="app">
        <div className="calculater">
         <div className="input">
-          <div id="seven" class="number">7</div>
-          <div id="eight"  class="number">8</div>
-          <div id="nine"  class="number">9</div>
-          <div id="minus"  class="operation">-</div>
-          <div id="seven" class="number">4</div>
-          <div id="eight"  class="number">5</div>
-          <div id="nine"  class="number">6</div>
-          <div id="minus"  class="operation">+</div>
-          <div id="one" class="number">1</div>
-          <div id="two"  class="number">2</div>
-          <div id="three"  class="number">3</div>
-          <div id="equal"  class="equal">=</div>
-          <div id="zero"  class="zero">0</div>
-          <div id="dot"  class="dot">.</div>
+          <div id="clear" className="clear">AC</div>
+          <div id="division" className="operation">/</div>
+          <div id="multiplication"  className="operation">×</div>
+          <div id="seven" className="number">7</div>
+          <div id="eight"  className="number">8</div>
+          <div id="nine"  className="number">9</div>
+          <div id="minus"  className="operation">-</div>
+          <div id="four" className="number">4</div>
+          <div id="five"  className="number">5</div>
+          <div id="six"  className="number">6</div>
+          <div id="minus" className="operation">+</div>
+          <div id="one" className="number">1</div>
+          <div id="two"  className="number">2</div>
+          <div id="three"  className="number">3</div>
+          <div id="equal"  className="equal">=</div>
+          <div id="zero"  className="zero">0</div>
+          <div id="dot"  className="dot">.</div>
 
         </div>
          
