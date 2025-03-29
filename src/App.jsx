ad9e7861597e2f81f43b9cef752f1d83a91db9c1
amjadsh92 <amjadsharafeddien@gmail.com>
2025-03-29 13:35:49 +0200

    Evaluate multiplication in mathematical expressions + delete the + sign at the beginning

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ return (
           
           <InputCell name="clear" type="clear" content="AC" setEquation = {setEquation} equation ={equation} />
           <InputCell name="division" type="operation" content="/" setEquation = {setEquation} equation ={equation} />
-          <InputCell name="multiplication" type="operation" content="×" setEquation = {setEquation} equation ={equation} />
+          <InputCell name="multiplication" type="operation" content="⋅" setEquation = {setEquation} equation ={equation} />
           <InputCell name="seven" type="number" content="7" setEquation = {setEquation} equation ={equation} />
           <InputCell name="eight" type="number" content="8" setEquation = {setEquation} equation ={equation} />
           <InputCell name="nine" type="number" content="9" setEquation = {setEquation} equation ={equation} />
@@ -68,15 +68,15 @@ function InputCell({name, type, content, setEquation, equation}){
       equation += content
       
       const regex1 = new RegExp(`\\${content}+`, "g")
-      const regex2 = new RegExp(`(?![/×]-)[×\\+\\-/]\\${content}`, "g");
+      const regex2 = new RegExp(`(?![/⋅]-)[⋅\\+\\-/]\\${content}`, "g");
       const regex3 = new RegExp(`^\\d+\\.\\${content}`);
-      const regex4 = new RegExp(`[×\\+\\-/]\\d+\\.\\${content}`, "g");
-      const regex5 = /×/;
+      const regex4 = new RegExp(`[⋅\\+\\-/]\\d+\\.\\${content}`, "g");
+      //const regex5 = /×/;
       equation = equation.replace(regex1,`${content}`);
       equation = equation.replace(regex2,`${content}`);
       equation = equation.replace(regex3,`0${content}`);
       equation = equation.replace(regex4,`${content}`);
-      equation = equation.replace(regex5,"⋅");
+      //equation = equation.replace(regex5,"⋅");
 
       setEquation(equation)
       }
@@ -84,7 +84,7 @@ function InputCell({name, type, content, setEquation, equation}){
       else if(type === "dot") {
         equation += content;
         const dotRegex1 = new RegExp(`\\${content}+`, "g")
-        const dotRegex2 = new RegExp(`^\\${content}|(?<=[+×/-])\\.` ,"g")
+        const dotRegex2 = new RegExp(`^\\${content}|(?<=[+⋅/-])\\.` ,"g")
         const dotRegex3 = new RegExp(`(?<=\\d+\\.\\d+)\\.`);
         equation = equation.replace(dotRegex1,`${content}`);
         equation = equation.replace(dotRegex2,`0${content}`);
@@ -93,30 +93,41 @@ function InputCell({name, type, content, setEquation, equation}){
 
       }
       else if(type === "equal"){
-        const equalRegex = /⋅/g;
-        const equalRegex1 = /^[/×].+/;
-        const equalRegex2 = /(?<=.+)[\.\+\/×]$/;
-        equation = equation.replace(equalRegex,"");
-        equationToEvaluate = equation.replace(equalRegex,"*")
+        debugger;
+        //const equalRegex = /⋅/g;
+        const equalRegex1 = /^[/⋅].+/;
+        const equalRegex2 = /(?<=.+)[\.\+\/⋅\*\-]$/;
+        const equalRegex3 = /^(\+)/;
+        //equation = equation.replace(equalRegex,"");
+        equationToEvaluate = equation.replace("⋅","*")
 
-        if (equation === "×" || equation === "/"){
+        if (equation === "⋅" || equation === "/"){
           equation += content + "NAN"
           setEquation(equation)
         }
         else if(equalRegex1.test(equation)){
+          if(equalRegex3.test(equation)){
+            equation = equation.replace(equalRegex3, "")
+          }
           setEquation(equation)
 
         }
         else if (equalRegex2.test(equation)){
+          if(equalRegex3.test(equation)){
+            equation = equation.replace(equalRegex3, "")
+          }
 
           equation = equation.replace(equalRegex2,"");
+          equationToEvaluate = equationToEvaluate.replace(equalRegex2,"");
           equation += content + eval(equationToEvaluate)
-
           setEquation(equation)
 
       }
 
       else{
+        if(equalRegex3.test(equation)){
+          equation = equation.replace(equalRegex3, "")
+        }
         equation += content + eval(equationToEvaluate)
         setEquation(equation)
 
@@ -134,7 +145,7 @@ function InputCell({name, type, content, setEquation, equation}){
 
   return(
 
-  <div id={name} className={type} onClick={handleClick}>{content}</div>
+  <div id={name} className={type} onClick={handleClick}>{name === "multiplication" ? "×" :content}</div>
 
   )
 
